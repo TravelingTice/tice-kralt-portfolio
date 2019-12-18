fc336b64b40b2d4ce8fb04f9d2568f5cfd456f16
Tice Kralt <34274090+TravelingTice@users.noreply.github.com>
2019-12-18 18:33:22 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/main.css';
+
+function App() {
+  return (
+    <div className="container">
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
