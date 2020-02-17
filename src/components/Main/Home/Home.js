79218cb170cd4bf3f0c66b4655318df5be992786
Matthijs Kralt <matthijs29@live.com>
2020-02-17 22:08:29 +0100

    Reconfigure home.js

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = props => (
   <section id="home">
     <div className="home-title">
-      <h1><span className="grey-text">T</span>ice Kralt</h1>
+      <h1>Tice Kralt</h1>
       <div>
-        <h2><span className="grey-text">F</span>ull-Stack Web Developer</h2>
+        <h2>Full-Stack Web Developer</h2>
       </div>
     </div>
   </section>
