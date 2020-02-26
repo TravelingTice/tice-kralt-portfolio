7e8fbae96492f7a953acab1cace5e129951a45cf
Matthijs Kralt <matthijs29@live.com>
2020-02-26 21:10:26 +0100

    Add ParallaxProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <ParallaxProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ParallaxProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
