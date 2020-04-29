4dcc0d702aaf06eb5f8234f7ac5b6ee779789733
TravelingTice <matthijs29@live.com>
2020-04-29 23:20:42 +0200

    Add footer

---
+++ components/footer/Footer.js
@@ -0,0 +1,25 @@
+const Footer = props => (
+  <footer>
+    <div className="social-icons">
+      <a href="https://github.com/TravelingTice">
+        <i className="fab fa-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/matthijs-kralt/">
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a href="https://www.facebook.com/matthijs.kralt1">
+        <i className="fab fa-facebook"></i>
+      </a>
+      <a href="https://twitter.com/travelingtice">
+        <i className="fab fa-twitter"></i>
+      </a>
+
+    </div>
+
+    <div className="copyright">
+      <p>2019 (c) Matthijs Kralt</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
