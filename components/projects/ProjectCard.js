a35f1342793bcbd4e9b5a2e63775e76f893f3445
TravelingTice <matthijs29@live.com>
2020-04-29 19:58:35 +0200

    add projectcard and projectgrid

---
+++ components/projects/ProjectCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import LinkBtn from '../Buttons/LinkBtn';
+
+const Card = styled.div`
+  background-color: white;
+  box-shadow: 1px 1px 7px rgba(0,0,0,.3);
+  cursor: pointer;
+  transition: transform 200ms cubic-bezier(.5,.07,.42,1.14), box-shadow 200ms ease-out;
+  display: grid;
+  grid-template-rows: 200px 1fr 40px;
+  &:hover {
+    transform: scale(1.02);
+    box-shadow: 2px 2px 15px rgba(0,0,0,.4);
+  }
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+  height: 100%;
+`;
+
+const CardText = styled.div`
+  display: none;
+  padding: 20px;
+  @media (min-width: 800px) {
+    display: block;
+  }
+`;
+
+const ProjectCard = ({ image, title, children, link }) => (
+  <Card>
+    <CardImage src={image} alt="" />
+
+    <CardText>
+      <h3>{title}</h3>
+      {children}
+    </CardText>
+    
+    <div>
+      <LinkBtn href={link} color="green" target="_blank">
+        View<i className="fas fa-angle-right angle-right"></i>
+      </LinkBtn>
+    </div>
+
+  </Card>
+);
+
+export default ProjectCard;
---
+++ components/projects/ProjectsGrid.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProjectCard from './ProjectCard';
+
+const Grid = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  padding: 25px;
+  grid-template-columns: 1fr 1fr;
+  @media (min-width: 740px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (min-width: 930px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+const ProjectGrid = () => {
+  return (
+    <Grid>
+
+      <ProjectCard
+      title="Product Database Quote Engine"
+      image="images/pdqeoffi.png"
+      link="https://pdqe-demo.herokapp.com">
+        An app that will allow you to <strong>quote</strong> your products to clients and send them a beautifully designed email in a couple clicks.
+      </ProjectCard>
+
+      <ProjectCard
+      title="PHAIWA Health Organisation Website"
+      image="images/phaiwa1.jpg"
+      link="https://phaiwa.org.au">
+        <strong>Public Health Advocacy Institute of Western Australia</strong>'s home page is made and designed by Tice Kralt. The site has put their online presence on a different level.
+      </ProjectCard>
+
+      <ProjectCard
+      title="Google Maps Tour Showcase"
+      image="images/googlemaps.jpg"
+      link="https://travelingtice.com/tour-map">
+        Showcase your roadtrip, bicycle tour or other travels on this awesome <strong>google maps</strong> API and javascript-made map with customizable markers and text windows.
+      </ProjectCard>
+
+      <ProjectCard
+      title="JaYoe Website"
+      image="images/jayoe.jpg"
+      link="https://jayoe.com">
+        <strong>Matt</strong> is cycling around the world in 10 years and this beautifully designed website wraps up his online presence. It contains a web store, google maps feature and more.
+      </ProjectCard>
+
+    </Grid>
+  )
+}
+
+export default ProjectGrid
