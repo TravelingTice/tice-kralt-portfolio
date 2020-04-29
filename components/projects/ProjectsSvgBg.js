512c300fc826b07982800723298aa3af24aa0396
TravelingTice <matthijs29@live.com>
2020-04-29 19:55:37 +0200

    add projectssvgbg

---
+++ components/projects/ProjectsSvgBg.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const PositionedDiv = styled.div`
+  position: absolute;
+  z-index: -1;
+  top: -53px;
+  width: 100%;
+  height: ${props => props.height}px;
+`;
+
+const ProjectsSvgBg = (props) => (
+  <PositionedDiv height={props.height}>
+    <svg height={props.height} width="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
+      <path  fill="#fcfcfc" d="M0 10 L35 0 L100 10 V100 H0 z" />
+    </svg>
+  </PositionedDiv>
+);
+
+export default ProjectsSvgBg;
