b250dc16742371e15ae1fc81801a32324a9f6243
TravelingTice <matthijs29@live.com>
2020-04-30 13:47:25 +0200

    Add arrows

--- components/projects/phone/Phone.js
+++ components/projects/phone/Phone.js
@@ -1,9 +1,32 @@
 import { useContext } from "react";
 import { ProjectsContext } from "../../../contexts/ProjectsContext";
 import { Button } from '@material-ui/core';
+import styled from 'styled-components';
+import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
+import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
+
+const ArrowDiv = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+`;
 
 const Phone = () => {
-  const { selectedProject } = useContext(ProjectsContext);
+  const { selectedProject, projects, isFirstProject, isLastProject } = useContext(ProjectsContext);
+
+  const leftArrow = () => !isFirstProject && (
+    <ArrowDiv onClick={}>
+      <ArrowBackIosRoundedIcon />
+    </ArrowDiv>
+  )
+
+  const rightArrow = () => !isLastProject && (
+    <ArrowDiv onClick={}>
+      <ArrowForwardIosRoundedIcon />
+    </ArrowDiv>
+  )
 
   const showDetails = () => selectedProject && (
     <div className="px-3">
@@ -14,7 +37,7 @@ const Phone = () => {
   )
 
   const showPhoneBg = () =>  (
-    <div style={{position: 'absolute', top: 11, left: 0, zIndex: -2, padding: 7, width: '100%', height: '100%'}}>
+    <div style={{position: 'absolute', top: 15, left: 0, zIndex: -2, padding: 7, width: '100%', height: '100%'}}>
       {selectedProject ? (
         <img style={{width: '100%'}} src={`/images${selectedProject.images[0]}`} alt={selectedProject.title} />
       ) : (
@@ -27,14 +50,17 @@ const Phone = () => {
 
   return (
     <>
-    <div className="d-flex justify-content-center">
+    <div style={{display: 'grid', gridTemplateColumns: '1fr auto 1fr' }}>
+      {leftArrow()}
       <div style={{width: '250px', position: 'relative'}}>
         <a href={selectedProject && selectedProject.url} target="_blank">
           <img style={{width: '100%'}} src="/shapes/iphonex.svg" alt="smartphone"/>
           {showPhoneBg()}
         </a>
       </div>
+      {rightArrow()}
     </div>
+
     <div className="d-flex justify-content-center text-center">
       {showDetails()}
     </div>
