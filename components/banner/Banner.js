a4e0ab6b2156059bd073af842f5bddcb1139c436
TravelingTice <matthijs29@live.com>
2020-05-01 15:22:43 +0200

    My first custom hook! React skills through the roof

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import { ParallaxBanner } from 'react-scroll-parallax';
+import { Container, Row, Col } from 'reactstrap';
+import sortBy from 'sort-by';
+import useViewportWidth from '../../hooks/useViewportWidth';
 
 const BannerBg = ({ height }) => (
   <ParallaxBanner
@@ -27,29 +30,59 @@ const BannerBg = ({ height }) => (
 const PositionedDiv = styled.div`
   position: absolute;
   bottom: 103px;
-  left: 18px;
   h1, h2 {
     display: inline-block;
     padding: 3px 10px;
     margin: 0;
     background-color: rgba(255,255,255,.4);
     font-size: 1.5em;
+    @media (min-width: 576px) {
+      font-size: 1.7em;
+    }
+    @media (min-width: 768px) {
+      font-size: 2em;
+    }
   }
 `;
 
 const Banner = () => {
-  const bannerHeight = 413;
+  const bannerHeights = [
+    { breakPoint: 0, height: 413 },
+    { breakPoint: 576, height: 500 },
+    { breakPoint: 768, height: 600 },
+    { breakPoint: 992, height: 500 }
+  ];
+
+  const { vw } = useViewportWidth();
+
+  const getBannerHeightForWidth = (width) => {
+    const sortedHeights = bannerHeights.sort(sortBy('-breakPoint'));
+
+    for (let i = 0; i < sortedHeights.length; i ++) {
+      const { breakPoint, height } = sortedHeights[i];
+      if (breakPoint < width) return height;
+    }
+  }
+
+  const bannerHeight = getBannerHeightForWidth(vw);
 
   return (
     <>
       <section style={{position: 'relative', height: bannerHeight}}>
         <PositionedDiv>
-          <div>
-            <h1>Tice Kralt</h1>
-          </div>
-          <div>
-            <h2>Full-stack web developer</h2>
-          </div>
+          <Container>
+            <Row>
+              <Col xs="12">
+                <div>
+                  <h1>Tice Kralt</h1>
+                </div>
+
+                <div>
+                  <h2>Full-stack web developer</h2>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </PositionedDiv>
       </section>
       <BannerBg height={bannerHeight}/>
---
+++ hooks/useViewportWidth.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useViewportWidth = () => {
+  const [vw, setVw] = useState(300);
+
+  useEffect(() => {
+    // set the vw with the current window viewport width
+    window.addEventListener('resize', () => {
+      setVw(window.innerWidth);
+    });
+
+    setVw(window.innerWidth);
+
+    return () => { window.removeEventListener('resize') };
+
+  }, []);
+
+  return { vw }
+}
+
+export default useViewportWidth;
