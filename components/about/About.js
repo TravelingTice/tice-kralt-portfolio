c97c13c760d5b7f0b7339a77be3f67e94af4410b
TravelingTice <matthijs29@live.com>
2020-05-01 12:28:22 +0200

    Use reactstrap in about me section

--- components/about/About.js
+++ components/about/About.js
@@ -1,23 +1,47 @@
+import styled from 'styled-components';
+import { Container, Row, Col } from 'reactstrap';
+
+const Avatar = styled.img`
+  width: 100px;
+  height: 100px;
+
+`;
+
+const AboutSvgTop = styled.img`
+  position: absolute;
+  top: -50px;
+  height: 50px;
+  width: 100%;
+  z-index: 0;
+`;
+
 const About = () => (
-  <section id="about" style={{backgroundColor: '#121a2b', color: '#fcfcfc', padding: '10px 0'}}>
+  <section id="about" style={{backgroundColor: '#0c111a', color: '#fcfcfc', padding: '10px 0', position: 'relative'}}>
     
-    <h2 className="my-4">About Me</h2>
+    <AboutSvgTop src="/shapes/aboutme-top.svg" alt="" />
 
-    <div className="about-me">
+    <Container fluid>
+      <Row>
+        <Col xs="12">
+          <h2 className="text-right">About Me</h2>
 
-      <img src="images/profileSQ.jpg" alt="" className="avatar img-fluid rounded-circle hoverable"/>
+          <Avatar src="images/avatar.jpg" alt="TravelingTice" />
+        </Col>
 
-      <div className="me-description">
+      </Row>
+    </Container>
+
+
+
+      <div>
         <h3>Matthijs Kralt</h3>
         <p>Driven full-stack web developer with 3 years experience as a freelancer in the field. Have worked with many international clients on various projects and was always able to meet their expectations. </p>
 
         <p>My strength lies in strong communication with my client during the workflow. I always make sure me and my client are on the same page about the project, to make sure that the end project is of the best quality and service.</p>
 
         <p>Prefer working with ReactJs and Rails API but my skills also include NodeJS, NextJS, Express, PostgreSQL and Ruby on Rails.</p>
-
       </div>
 
-    </div>
 
   </section>
 );
