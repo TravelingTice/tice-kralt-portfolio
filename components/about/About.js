865ca03a227461c28a533cf83987859825024bf9
TravelingTice <matthijs29@live.com>
2020-05-01 12:10:03 +0200

    Add better text to about me

--- components/about/About.js
+++ components/about/About.js
@@ -1,5 +1,5 @@
 const About = () => (
-  <section id="about" style={{backgroundColor: '#2B303A', color: '#fcfcfc', padding: '10px 0'}}>
+  <section id="about" style={{backgroundColor: '#121a2b', color: '#fcfcfc', padding: '10px 0'}}>
     
     <h2 className="my-4">About Me</h2>
 
@@ -9,7 +9,12 @@ const About = () => (
 
       <div className="me-description">
         <h3>Matthijs Kralt</h3>
-        <p>I love helping people to realise their dreams and imaginations with my skills in web programming and development and talent for communication and listening. My passion is to use my incredible opportunity to make a meaningful difference in this world.</p>
+        <p>Driven full-stack web developer with 3 years experience as a freelancer in the field. Have worked with many international clients on various projects and was always able to meet their expectations. </p>
+
+        <p>My strength lies in strong communication with my client during the workflow. I always make sure me and my client are on the same page about the project, to make sure that the end project is of the best quality and service.</p>
+
+        <p>Prefer working with ReactJs and Rails API but my skills also include NodeJS, NextJS, Express, PostgreSQL and Ruby on Rails.</p>
+
       </div>
 
     </div>
